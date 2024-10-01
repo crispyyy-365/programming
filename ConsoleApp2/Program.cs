1895b313e59026048a0d4e9505f24d07102359be
crispyyy-365 <huseyna-bp217@code.edu.az>
2024-10-01 18:55:57 +0400

    it starts with

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,102 @@
+﻿//int myNum1 = 4;
+//int myNum2 = 8;
+
+//if (myNum1 % 2 == 0 && myNum2 % 2 == 0)
+//{
+//    Console.WriteLine(myNum1 + " " + myNum2);
+//    Console.WriteLine(myNum1 + myNum2);
+//}
+//else
+//{
+//    Console.WriteLine("cut eded deyil");
+//}
+
+//Console.WriteLine("bali daxil edin");
+//int mynum = Convert.ToInt32(Console.ReadLine());
+
+//if (mynum > 100 || mynum < 0)
+//{
+//    Console.WriteLine("qiymetlendirme [0;100] intervalinda aparilir !");
+//};
+//if (mynum >= 0 && mynum <= 20)
+//{
+//    Console.WriteLine("berbad");
+//}
+//else if (mynum > 20 && mynum <= 40)
+//{
+//    Console.WriteLine("pis");
+//}
+//else if (mynum > 40 && mynum <= 60)
+//{
+//    Console.WriteLine("kafi");
+//}
+//else if (mynum > 60 && mynum <= 80)
+//{
+//    Console.WriteLine("yaxsi");
+//}
+//else if (mynum > 80 && mynum <= 100)
+//{
+//    Console.WriteLine("ela !");
+//};
+//Console.WriteLine("ay :");
+//string month = Console.ReadLine();
+//if (month == "Dekabr" || month == "Yanvar" || month == "Fevral")
+//{
+//    Console.WriteLine("Qis feslidir.");
+//}
+//else if (month == "Mart" || month == "Aprel" || month == "May")
+//{
+//    Console.WriteLine(" feslidir.");
+//}
+//else if (month == "Mart" || month == "Aprel" || month == "May")
+//{
+//    Console.WriteLine("Yaz feslidir.");
+//}
+//else if (month == "Iyun" || month == "Iyul" || month == "Avqust")
+//{
+//    Console.WriteLine("Yay feslidir.");
+//}
+//else if (month == "Sentyabr" || month == "Oktyabr" || month == "Noyabr")
+//{
+//    Console.WriteLine("Payiz feslidir.");
+//}
+//else
+//{
+//    Console.WriteLine("bele bir fesl yoxdur !");
+//};
+//Console.WriteLine("heftenin gununu daxil et : ");
+//string d = Console.ReadLine();
+
+//if (d == "bazar ertesi")
+//{
+//    Console.WriteLine("1-ci gun.");
+//}
+//else if (d == "cersenbe axsami")
+//{
+//    Console.WriteLine("2-ci gun");
+//}
+//else if (d == "cersenbe")
+//{
+//    Console.WriteLine("3-cu gun");
+//}
+//else if (d == "cume axsami")
+//{
+//    Console.WriteLine("4-cu gun");
+//}
+//else if (d == "cume")
+//{
+//    Console.WriteLine("5-ci gun");
+//}
+//else if (d == "senbe")
+//{
+//    Console.WriteLine("6-ci gun");
+//}
+//else if (d == "bazar")
+//{
+
+//    Console.WriteLine();
+//}
+//else
+//{
+//    Console.WriteLine("heftenin bele bir gunu yoxdur !");
+//};
